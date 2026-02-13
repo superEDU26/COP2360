4a3dd48317d31a12ee8d6192f487ae8e90665c6e
superC12 <christopherandrewr@gmail.com>
2026-02-13 17:32:35 -0500

    Added Stock class with Worth property

---
+++ Module3stockExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Stock
+{
+    // Automatic properties
+    public decimal CurrentPrice { get; set; }
+    public decimal SharesOwned { get; set; }
+
+    // Read-only calculated property
+    public decimal Worth
+    {
+        get { return CurrentPrice * SharesOwned; }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Creating an instance of the class
+        Stock myStock = new Stock();
+
+        // Setting property values
+        myStock.CurrentPrice = 50m;
+        myStock.SharesOwned = 100m;
+
+        // Displaying result
+        Console.WriteLine("Stock worth: " + myStock.Worth);
+    }
+}
